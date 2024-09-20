24f7b7fe1ffa2a5028dade088d97aef0d6f3d208
Бакшеев Сергей <sergey.baksheev@tele2.ru>
2024-09-20 15:34:33 +0300

    Добавление функций калькулятора, тесты

--- lib/education.ex
+++ lib/education.ex
@@ -1,9 +1,50 @@
 defmodule Education do
   @moduledoc """
-  Education keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+    module for education purposes
   """
+
+  @doc """
+    Calculate operations with 2 params
+
+    iex> Education.calc("sum", 2, 3)
+    { :ok, 5 }
+
+    iex> Education.calc("multiplication", 2, 3)
+    { :ok, 6 }
+
+    iex> Education.calc("subtraction", 2, 3)
+    { :ok, -1 }
+
+    iex> Education.calc("division", 6, 3)
+    { :ok, 2.0 }
+
+    iex> Education.calc("bad", 6, 3)
+    {:error, "no valid operation: bad"}
+
+    iex> Education.calc("sum", "str", 3)
+    ** (ArithmeticError) bad argument in arithmetic expression
+  """
+  def calc(operation, num1, num2) do
+    case operation do
+      "sum" ->
+        sum(num1, num2)
+      "multiplication" ->
+        multiplication(num1, num2)
+      "subtraction" ->
+        sum(num1, -num2)
+      "division" ->
+        multiplication(num1, 1/num2)
+      _ ->
+        {:error, "no valid operation: #{operation}"}
+    end
+  end
+
+  defp sum(a, b) do
+    {:ok, a + b}
+  end
+
+  defp multiplication(a, b) do
+    {:ok, a * b}
+  end
+
 end
