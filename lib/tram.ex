91b11fc459918f778f6cce8c908818ec534d322c
Бакшеев Сергей <sergey.baksheev@tele2.ru>
2024-10-10 17:04:59 +0300

    Tram state machine

--- lib/tram.ex
+++ lib/tram.ex
@@ -2,6 +2,11 @@ defmodule Tram do
   @moduledoc """
     finite state machine
     depo, move, stop, open_doors
+
+    Tramp.start_link
+    Tramp.move()
+    Tramp.stop()
+    Tramp.open_doors
   """
   use GenServer
 
@@ -10,19 +15,22 @@ defmodule Tram do
     {:ok, initial_state}
   end
 
+  def start_link(initial_state \\ :depo) do
+    GenServer.start_link(__MODULE__, initial_state, name: __MODULE__)
+  end
+
   def legal_transition?(current_state, new_state) do
-    paths = %{
-      depo: [:move],
-      move: [:depo, :stop],
-      stop: [:move, :open_doors],
-      open_doors: [:stop]
-    }
+    paths = %{depo: [:move],move: [:depo, :stop],stop: [:move, :open_doors],open_doors: [:stop]}
     new_state in paths[current_state]
   end
 
   def change_state(new_state) do
     current_state = current()
-    GenServer.cast(__MODULE__, new_state) if legal_transition?(current_state, new_state)
+    if legal_transition?(current_state, new_state) do
+      {GenServer.cast(__MODULE__, new_state), "state changed"}
+    else
+      {:error, "Wrong state for #{current_state}"}
+    end
   end
 
   # check state
@@ -35,11 +43,13 @@ defmodule Tram do
   def open_doors, do: change_state(:open_doors)
   def close_doors, do: change_state(:stop)
 
+  @impl true
   def handle_call(:current, _from, state) do
     {:reply, state, state}
   end
 
-  def handle_cast(new_state, state) do
+  @impl true
+  def handle_cast(new_state, _state) do
     {:noreply, new_state}
   end
 end
