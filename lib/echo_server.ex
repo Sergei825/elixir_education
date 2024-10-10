533a269dac18e2b400ea93143963d2c97b4fd89a
Бакшеев Сергей <sergey.baksheev@tele2.ru>
2024-10-10 13:51:43 +0300

    Правки по комментариям

--- lib/echo_server.ex
+++ lib/echo_server.ex
@@ -2,7 +2,15 @@ defmodule EchoServer do
   @moduledoc """
     module for ping pong testing
   """
-  def ping(pid) do
-    send(pid, {:pong, node()})
+  use GenServer
+
+  @impl true
+  def init(initial_state) do
+    {:ok, initial_state}
+  end
+
+  @impl true
+  def handle_call(:ping, _from, state) do
+    {:reply, {:pong, node()}, state}
   end
 end
--- test/echo_server_test.exs
+++ test/echo_server_test.exs
@@ -1,8 +1,10 @@
 defmodule EchoServerTest do
   use ExUnit.Case
 
-  test "assert_receive/1" do
-    EchoServer.ping(self())
-    assert_receive {:pong, :nonode@nohost}
+  test "ping" do
+    {_status, pid} = GenServer.start_link(EchoServer, "1, 2, 3")
+    {word, node} = GenServer.call(pid, :ping)
+    IO.puts "#{word} + #{node}"
+    assert word == :pong
   end
 end
